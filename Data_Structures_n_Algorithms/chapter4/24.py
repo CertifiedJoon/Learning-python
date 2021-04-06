03c867bc224f6c06b624731b5e7c6ac8a0e7b0f0
CertifiedJoon <u3566665@connect.hku.hk>
2021-04-06 10:55:22 +0000

    working on cryptarithmetic solver

--- Data_Structures_n_Algorithms/chapter4/24.py
+++ Data_Structures_n_Algorithms/chapter4/24.py
@@ -1,12 +1,55 @@
-import os
+class Puzzle:
+    def __init__(self, first, second, result): # takes three strings as input
+        self._letters = [c for c in first] + [c for c in second] + [c for c in result] # stores all three sgrings into one list
+        self._map = {k : -1 for k in self._letters}
+        self._len1 = len(first)
+        self._len2 = len(second)
+        self._len3 = len(result)
 
-def FindFile(path, filename):
-    if os.path.isdir(path):
-        for fd in os.listdir(path):
-            childpath = os.path.join(path, fd)
-            if (os.path.isdir(childpath)):
-                FindFile(childpath, filename)
-            elif (fd == filename):
-                print(childpath)
-                
-FindFile('/workspace/Learning-python/Data_Structures_n_Algorithms', '20.py')
+        
+    def _ltoi(self, lst): #converts a list to integer
+        ret = 0
+        for i in lst:
+            ret = ret * 10 + int(i)
+        return ret
+    
+    def assign_letter(self, letter, num): #assigns a 'num'
+        if (num in self._map.values()):
+            return False
+        self._map[letter] = num
+        
+        return True
+        
+    def unassign_letter(self, letter):
+        self._map[letter] = -1
+        
+    def is_solved(self):
+        for i in range (len(self._letters)):
+            self._letters[i] = str(self._map[self._letters[i]])
+        if (self._ltoi(self._letters[:self._len1]) + self._ltoi(self._letters[self._len1:self._len1+self._len2])) == self._ltoi(self._letters[self._len1+self._len2:]):
+                return True
+        return False
+
+    def print_letters(self):
+        print(self._letters[:self._len1])
+        print(self._letters[self._len1:self._len1+self._len2])
+        print(self._letters[self._len1+self._len2:])
+    
+def Cryptarithmetic(p, letters_to_assign):
+    if (letters_to_assign == ''):
+        if (p.is_solved()):
+            return True
+        return False
+    
+    for i in range (10):
+        if p.assign_letter(letters_to_assign[0], i):
+            if (Cryptarithmetic(p, letters_to_assign[1:])):
+                return True
+            else:
+                p.unassign_letter(letters_to_assign[0])
+    
+    return False
+
+p = Puzzle('pot', 'pan', 'bib')
+Cryptarithmetic(p, 'potanbi')
+p.print_letters()
